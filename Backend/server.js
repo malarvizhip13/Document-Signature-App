ab23de455f175f5a2a93d9701bc31be99e54c7d7
malarvizhi <malarvizhi.p8248@gmail.com>
2026-06-06 21:29:16 +0530

    day 1

---
+++ Backend/server.js
@@ -0,0 +1,7 @@
+ const express =require('express');
+   const cors = require('cors');
+   const mongoose = require('mongoose');
+   const app =express();
+
+   app.use(cors());
+   app.use(express.urlencoded({extended:true}))

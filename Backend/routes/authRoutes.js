f663a5555131f1cb3f726996ae29824211135cf6
malarvizhi <malarvizhi.p8248@gmail.com>
2026-06-11 19:29:03 +0530

    day 22

---
+++ Backend/routes/authRoutes.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const User = require("../model/User");
+
+const router = express.Router();
+
+router.get("/test", (req, res) => {
+    res.send("Auth Route Working");
+});
+
+
+router.post("/register", async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({
+        message: "User already exists",
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    res.status(201).json({
+      message: "User Registered Successfully",
+      user,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+});
+module.exports = router;
--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,23 @@
  const express =require('express');
    const cors = require('cors');
    const mongoose = require('mongoose');
-   const app =express();
+   require ('dotenv').config();
 
-   app.use(cors());
+   const authRoutes = require("./routes/authRoutes");
+   const app =express();
+   const mongoDB=process.env.mongoDB_URL;
+   
+app.use(cors());
    app.use(express.urlencoded({extended:true}))
+app.use(express.json());
+   app.use("/api/auth", authRoutes);
+    mongoose.connect(mongoDB)
+    .then(()=>{
+   console.log('mongodb connected')
+})
+.catch(()=>{
+console.log('no connction')
+})
+   app.listen(5000,()=>{
+    console.log('server running')
+   })
